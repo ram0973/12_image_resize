57dd3cf134bbea39e3412734fa75c9b6330a225b
ram0973 <ram0973@gmail.com>
2016-11-03 07:07:33 +0300

    перенес resize выше, чтобы учесть ValueException

--- image_resize.py
+++ image_resize.py
@@ -67,10 +67,9 @@ def resize_image(infile: str, width: int, height: int, scale: float,
             elif height:
                 height = abs(int(height))
                 width = round(height * (img.width / img.height))
+        tmp = img.resize((width, height), Image.ANTIALIAS)
     except TypeError or ValueError or ArithmeticError:
         return IMAGE_RESIZE_BAD_PARAMS, None
-
-    tmp = img.resize((width, height), Image.ANTIALIAS)
 
     name, ext = os.path.splitext(infile)
     if not outfile:

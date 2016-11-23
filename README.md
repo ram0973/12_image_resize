780a20ba5f1a49f705b11bba54cb0c4612e3730d
ram0973 <ram0973@gmail.com>
2016-11-23 20:30:32 +0300

    mass fixes

--- README.md
+++ README.md
@@ -1,86 +1,27 @@
-# Решение задачи [№12](https://devman.org/challenges/12/) с сайта [devman.org](https://devman.org)
-
-## Условие задачи:
-
-Написать скрипт, который принимает на вход изображение и кладёт 
-изображение с новым размером, куда скажет пользователь или рядом с исходным. 
-У него есть обязательный аргумент – путь до исходной картинки. 
-И несколько необязательных: 
-width - ширина результирующей картинки, 
-height - её высота, 
-scale - во сколько раз увеличить изображение (может быть меньше 1), 
-output - куда класть результирующий файл.
-
-Логика работы такая:
-
-Если указана только ширина – высота считается так, чтобы сохранить пропорции изображения. 
-И наоборот, если указана и ширина и высота – создать именно такое изображение. 
-Вывести в консоль предупреждение, если пропорции не совпадают с исходным изображением.
-Если указан масштаб, то ширина и высота указаны быть не могут. 
-Иначе никакого ресайза не происходит и скрипт ломается с понятной ошибкой.
-Если не указан путь до финального файла, то результат кладётся рядом с 
-исходным файлом. Если исходный файл называется pic.jpg (100x200),
- то после вызова python image_resize.py --scale 2 pic.jpg должен 
- появиться файл pic__200x400.jpg.
-
-Для работы с изображениями можно использовать Pillow, а для работы с 
-аргументами скрипта – модуль argparse (он входит в стандартную библиотеку 
-и его не нужно устанавливать).
-
-Скрипт должен работать хотя бы с jpg и png.
-
-## Системные требования
-
+# Task [№12](https://devman.org/challenges/12/) from [devman](https://devman.org)
+## Requirements
 ```
 Python 3.5.2+
-Внешний модуль win-unicode-console
-Внешний модуль pillow
+pillow
 ```
-
-## Установка
-
-Windows
-
-```    
-git clone https://github.com/ram0973/12_image_resize.git
-cd 12_image_resize
-pip install -r requirements.txt
-```
-
-Linux
+## Setup
 ```    
 git clone https://github.com/ram0973/12_image_resize.git
 cd 12_image_resize
 pip3 install -r requirements.txt
 ```
-    
-## Описание работы
-
-Пользователь вводит обязательный аргумент - путь до исходной картинки и 
-четыре необязательных. Скрипт изменяет размеры картинки и сохраняет, как 
-указано в условии задачи. Работает как минимум с jpg, png, tif, bmp, tga. 
-В gif сохраняет, но получившийся gif не может обработать.
-
-## Запуск
-
-Windows
-
-```
-python image_resize.py --i путь_до_исходной_картинки
-```
- 
-Linux
-
+## Description
+The user enters the required argument - the path to the original image and
+four optional. The script resizes the images and saves as
+specified in the task. It works at least with jpg, png, tif, bmp, tga.
+Saves to gif, but can't handle the resulting gif.
+## Usage
 ``` 
-python3 image_resize.py --i путь_до_исходной_картинки 
+python3 image_resize.py --i image_path 
 ```
-
-Пример необязательных параметров:
+Example of optional parameters:
 ``` 
 python3 image_resize.py --i original_image.jpg --w 200 --h 100 --s 0.5 --o image_scaled.jpg
 ```
-
- 
-## Лицензия
-
+## License
 [MIT](http://opensource.org/licenses/MIT)
--- image_resize.py
+++ image_resize.py
@@ -1,125 +1,60 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
-import sys
 from PIL import Image
 
-IMAGE_RESIZE_OK = 0
-IMAGE_RESIZE_BAD_PARAMS = 1
-IMAGE_RESIZE_WRONG_OUTPUT_FORMAT = 2
-IMAGE_RESIZE_BAD_INPUT_IMAGE = 3
+
+def calc_new_image_size(img, width, height, scale):
+    if scale:
+        scale = abs(float(scale))
+        width = round(img.width * scale)
+        height = round(img.height * scale)
+    else:
+        if width:
+            width = abs(int(width))
+            height = round(width / (img.width / img.height))
+        elif height:
+            height = abs(int(height))
+            width = round(height * (img.width / img.height))
+    return width, height
 
 
-def enable_win_unicode_console():
-    """
-    Включаем правильное отображение unicode в консоли под MS Windows
-    """
-    if sys.platform == 'win32':
-        import win_unicode_console
-        win_unicode_console.enable()
-
-
-def get_args_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--infile', '--i', help='Путь исходного файла')
-    # знаю про required=True, но сообщение об ошибке при этом не очень понятное
-    parser.add_argument('--width', '--w', help='Ширина конечной картинки',
-                        type=int)
-    parser.add_argument('--height', '--h', help='Высота конечной картинки',
-                        type=int)
-    parser.add_argument('--scale', '--s', help='Масштаб конечной картинки',
-                        type=float)
-    parser.add_argument('--outfile', '--o', help='Путь конечного файла')
-
-    return parser
-
-
-def resize_image(infile: str, width: int, height: int, scale: float,
-                 outfile: str) -> (int, str):
-    """
-    Изменение размеров изображений по заданным параметрам
-    :param infile: путь к входной картинка
-    :param width: конечная ширина
-    :param height: конечная высота
-    :param scale: масштаб, может быть меньше 1
-    :param outfile: путь к выходной картинке
-    :return: код ошибки, название конечного файла
-    """
-    if width == 0 or height == 0 or scale == .0:
-        return IMAGE_RESIZE_BAD_PARAMS, None
-    if scale and (width or height):
-        return IMAGE_RESIZE_BAD_PARAMS, None
-
-    try:
-        img = Image.open(infile)
-    except IOError:
-        return IMAGE_RESIZE_BAD_INPUT_IMAGE, None
-
-    try:
-        if scale:
-            scale = abs(float(scale))
-            width = round(img.width * scale)
-            height = round(img.height * scale)
-        else:
-            if width:
-                width = abs(int(width))
-                height = round(width / (img.width / img.height))
-            elif height:
-                height = abs(int(height))
-                width = round(height * (img.width / img.height))
-        tmp = img.resize((width, height), Image.ANTIALIAS)
-    except (TypeError, ValueError, ArithmeticError, OverflowError):
-        return IMAGE_RESIZE_BAD_PARAMS, None
-
+def resize_image(infile, width, height, scale, outfile):
+    img = Image.open(infile)
+    width, height = calc_new_image_size(img, width, height, scale)
+    tmp = img.resize((width, height), Image.ANTIALIAS)
     name, ext = os.path.splitext(infile)
     if not outfile:
         outfile = '{0}__{1}x{2}{3}'.format(name, tmp.width, tmp.height, ext)
-    try:
-        tmp.save(outfile)
-    except KeyError:  # Невозможно определить формат по имени выходного файла
-        return IMAGE_RESIZE_WRONG_OUTPUT_FORMAT, None
-    finally:
-        tmp.close()
+    tmp.save(outfile)
+    tmp.close()
+    return outfile
 
-    return IMAGE_RESIZE_OK, outfile
+
+def get_console_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--infile', '--i', help='Input image', required=True)
+    parser.add_argument('--width', '--w', help='Final image width', type=int)
+    parser.add_argument('--height', '--h', help='Final image height', type=int)
+    parser.add_argument('--scale', '--s', help='Final image scale', type=float)
+    parser.add_argument('--outfile', '--o', help='Output image')
+    args = parser.parse_args()
+    if args.scale and (args.width or args.height):
+        print('You can\'t specify both the scale and the width or height')
+        parser.print_usage()
+        exit(1)
+    elif not args.width and not args.height and not args.scale:
+        print('You did\'nt specify any width or height or the scale')
+        parser.print_usage()
+        exit(1)
+    return args
 
 
 def main():
-    enable_win_unicode_console()
-
-    args_parser = get_args_parser()
-    args = get_args_parser().parse_args()
-
-    if not args.infile:
-        print('Вы не задали путь к исходной картинке')
-        args_parser.print_usage()
-        exit(1)
-    if args.scale and (args.width or args.height):
-        print('Нельзя одновременно задавать масштаб и ширину или высоту')
-        args_parser.print_usage()
-        exit(1)
-    elif not args.width and not args.height and not args.scale:
-        print('Вы не задали ни ширину, ни высоту, ни масштаб')
-        args_parser.print_usage()
-        exit(1)
-
-    try:
-        error, final_file = resize_image(args.infile, args.width, args.height,
-                                         args.scale, args.outfile)
-        if error:
-            if error == IMAGE_RESIZE_BAD_PARAMS:
-                print('Ошибка: неверные параметры')
-            elif error == IMAGE_RESIZE_BAD_INPUT_IMAGE:
-                print('Ошибка: Исходное изображение некорректно')
-            elif error == IMAGE_RESIZE_WRONG_OUTPUT_FORMAT:
-                print('Ошибка: Имя конечного файла некорректно')
-            exit(1)
-        else:
-            print('Файл обработан и сохранён как %s' % final_file)
-    except OSError as error:
-        print('Ошибка: %s в файле: %s' % (error.strerror, error.filename))
-        exit(1)
-
+    args = get_console_args()
+    final_file = resize_image(args.infile, args.width, args.height,
+                              args.scale, args.outfile)
+    print('The file is processed and saved as %s' % final_file)
 
 if __name__ == '__main__':
     main()
